4ec1d0d135d6064467eb461f9e34b2baec50daaa
Nitish Puri <npuri1903@gmail.com>
2017-06-15 03:26:34 +0530

    Backpropogation implementation for gradient calculations.

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -67,12 +67,14 @@ Theta2_grad = zeros(size(Theta2));
 X = [ones(m, 1) X];
 
 % Calculate activation for second layer
-a2 = sigmoid(X*Theta1');
+z2 = X*Theta1';
+a2 = sigmoid(z2);
 % Add constant/bias node to second layer
 a2 = [ones(m,1) a2];
 
-% Calculate activation for third layer
-a3 = sigmoid(a2*Theta2');
+% Calculate activation for third layer, a3 = h, hypothesis
+z3 = a2*Theta2';
+a3 = sigmoid(z3);
 
 % Number of output nodes for final layer = number of classes.
 k = size(Theta2, 1);
@@ -89,9 +91,59 @@ end
 J = (-1/m)*sum((yt.*log(a3) + (1 - yt).*log(1 - a3))(:));
 
 % Regularzation
-JR = (lambda/(2*m))*(sum((Theta1(:, 2:size(Theta1, 2)).^2)(:)) + sum((Theta2(:, 2:size(Theta2, 2)).^2)(:)) );
+JR = (lambda/(2*m))*(sum((Theta1(:, 2:end).^2)(:)) + sum((Theta2(:, 2:end).^2)(:)) );
 
 J += JR;
+
+
+%% Backpropogation
+
+% Error term for layer 3
+d3 = a3 - yt;  % 5000 X 10
+
+% Error term for layer 2
+d2 = (d3*Theta2); % 5000 X 26
+d2 = d2(:, 2:end).*sigmoidGradient(z2);   % 5000 X 25
+
+% Gradient accumulation
+Theta2_grad = (1/m)*(d3'*a2);
+Theta1_grad = (1/m)*(d2'*X);
+
+% Selectors for removing 'bias' terms.
+selector2 = ones(size(Theta2));
+selector2(:, 1) = 0;
+selector1 = ones(size(Theta1));
+selector1(:, 1) = 0;
+
+% Regularization of gradients
+Theta2_grad += (lambda/m)*(Theta2.*selector2);
+Theta1_grad += (lambda/m)*(Theta1.*selector1);
+
+% for t = 1:m
+%     a1 = X(t, :);  % 1 X 401
+%     z2 = a1*Theta1';  % 1 X 25
+%     a2 = sigmoid(z2);  % 1 X 25
+%     a2 = [1 a2]; % 1 X 26
+%     z3 = a2*Theta2';   % 1 X 10 
+%     a3 = sigmoid(z3);  % 1 X 10
+%     yt = zeros(1, k); % 1 X 10
+%     yt(:, y(t)) = 1; % 1 X 10
+    
+%     d3 = a3 - yt;  % 1 X 10
+
+%     d2 = (d3*Theta2);   % 1 X 26
+%     d2 = d2(:, 2:end);  % 1 X 25
+%     d2 = d2.*sigmoidGradient(z2);   % 1 X 25
+
+%     Theta2_grad += d3'*a2;  % 10 X 26
+%     Theta1_grad += d2'*a1;  % 25 X 401
+
+% end
+
+
+% Theta1_grad = (1/m)*Theta1_grad;
+% Theta2_grad = (1/m)*Theta2_grad;
+
 
 
 % -------------------------------------------------------------
